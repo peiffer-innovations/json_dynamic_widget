799b62d1e5bd542059edb7ebc26263c64e752bba
limonadev <limonadev@whiteleaf.dev>
2020-08-20 18:00:14 -0500

    Added clip_oval tests

---
+++ test/builders/json_clip_oval_build_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+
+void main() {
+  test('type', () {
+    const type = JsonClipOvalBuilder.type;
+
+    expect(type, 'clip_oval');
+    expect(JsonWidgetRegistry.instance.getWidgetBuilder(type) != null, true);
+    expect(
+      JsonWidgetRegistry.instance.getWidgetBuilder(type)({})
+          is JsonClipOvalBuilder,
+      true,
+    );
+  });
+}
