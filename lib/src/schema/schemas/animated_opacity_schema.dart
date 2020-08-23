8a79daa14ad740978758db1d6c1bef3fdb45ba2c
limonadev <limonadev@whiteleaf.dev>
2020-08-23 17:45:41 -0500

    Added basic schema and builder for AnimatedOpacity

---
+++ lib/src/schema/schemas/animated_opacity_schema.dart
@@ -0,0 +1,21 @@
+import 'package:json_theme/json_theme_schemas.dart';
+
+class AnimatedOpacitySchema {
+  static const id =
+      'https://peifferinnovations.com/json_dynamic_widget/animated_opacity_schema';
+
+  static final schema = {
+    r'$schema': 'http://json-schema.org/draft-06/schema#',
+    r'$id': '$id',
+    'type': 'object',
+    'title': 'AnimatedOpacityBuilder',
+    'additionalProperties': false,
+    'properties': {
+      'alwaysIncludeSemantics': SchemaHelper.boolSchema,
+      'curve': SchemaHelper.stringSchema,
+      'duration': SchemaHelper.numberSchema,
+      'onEnd': SchemaHelper.stringSchema,
+      'opacity': SchemaHelper.stringSchema,
+    },
+  };
+}
