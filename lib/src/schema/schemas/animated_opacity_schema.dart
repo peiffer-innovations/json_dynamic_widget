8ccc5d534d2eb0275e7cab5e9b34975770fc4e51
limonadev <limonadev@whiteleaf.dev>
2020-08-24 16:27:22 -0500

    Fixed AnimatedOpacitySchema id

--- lib/src/schema/schemas/animated_opacity_schema.dart
+++ lib/src/schema/schemas/animated_opacity_schema.dart
@@ -2,7 +2,7 @@ import 'package:json_theme/json_theme_schemas.dart';
 
 class AnimatedOpacitySchema {
   static const id =
-      'https://peifferinnovations.com/json_dynamic_widget/animated_opacity_schema';
+      'https://peifferinnovations.com/json_dynamic_widget/schemas/animated_opacity';
 
   static final schema = {
     r'$schema': 'http://json-schema.org/draft-06/schema#',
