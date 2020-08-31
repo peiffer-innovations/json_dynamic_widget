82f13e824949c149517885e8bc04c27fbca3b1df
limonadev <limonadev@whiteleaf.dev>
2020-08-31 12:08:39 -0500

    Added new widget tests

---
+++ test/builders/json_animated_physical_model_builder_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+
+void main() {
+  test('type', () {
+    const type = JsonAnimatedPhysicalModelBuilder.type;
+
+    expect(type, 'animated_physical_model');
+    expect(JsonWidgetRegistry.instance.getWidgetBuilder(type) != null, true);
+    expect(
+      JsonWidgetRegistry.instance.getWidgetBuilder(type)(
+        {
+          'color': '#FFF',
+          'duration': 1000,
+          'elevation': 0,
+          'shadowColor': '#000',
+          'shape': 'rectangle',
+        },
+      ) is JsonAnimatedPhysicalModelBuilder,
+      true,
+    );
+  });
+}
