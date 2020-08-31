db42950676e3168d3a2c9cf0e451f0c253ba21c9
limonadev <limonadev@whiteleaf.dev>
2020-08-31 12:00:50 -0500

    Added new widget to registry

--- lib/src/schema/all.dart
+++ lib/src/schema/all.dart
@@ -3,6 +3,7 @@ export 'schemas/animated_align_schema.dart';
 export 'schemas/animated_default_text_style_schema.dart';
 export 'schemas/animated_opacity_schema.dart';
 export 'schemas/animated_padding_schema.dart';
+export 'schemas/animated_physical_model_schema.dart';
 export 'schemas/app_bar_schema.dart';
 export 'schemas/aspect_ratio_schema.dart';
 export 'schemas/asset_image_schema.dart';
