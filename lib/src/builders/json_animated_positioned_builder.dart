e6e45e559eac78d740c4c700db507a40d8b052cf
limonadev <limonadev@whiteleaf.dev>
2020-09-01 10:53:09 -0500

    Fixed docs classes names

--- lib/src/builders/json_animated_positioned_builder.dart
+++ lib/src/builders/json_animated_positioned_builder.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:json_class/json_class.dart';
 import 'package:json_dynamic_widget/json_dynamic_widget.dart';
 
-/// Builder that can build a [JsonAnimatedPositionedBuilder] widget.
+/// Builder that can build an [AnimatedPositioned] widget.
 /// See the [fromDynamic] for the format.
 class JsonAnimatedPositionedBuilder extends JsonWidgetBuilder {
   JsonAnimatedPositionedBuilder({
