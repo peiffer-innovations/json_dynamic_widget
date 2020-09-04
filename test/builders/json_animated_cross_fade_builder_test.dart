46727bb90e571832e60416992e44c79b3f38b158
limonadev <limonadev@whiteleaf.dev>
2020-09-04 11:24:21 -0500

    Added new widget tests

---
+++ test/builders/json_animated_cross_fade_builder_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+
+void main() {
+  test('type', () {
+    const type = JsonAnimatedCrossFadeBuilder.type;
+
+    expect(type, 'animated_cross_fade');
+    expect(JsonWidgetRegistry.instance.getWidgetBuilder(type) != null, true);
+    expect(
+      JsonWidgetRegistry.instance.getWidgetBuilder(type)(
+        {
+          'crossFadeState': 'showFirst',
+          'duration': 1000,
+        },
+      ) is JsonAnimatedCrossFadeBuilder,
+      true,
+    );
+  });
+}
