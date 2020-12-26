b16c1fbd723068e11b791e8b825af7d46cf2266f
Jeff Peiffer <jpeiffer@gmail.com>
2020-12-26 17:16:35 -0500

    Updating to 1.4

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,17 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "9e655c2a10bad2d6247703c1a29199e5",
-"/": "9e655c2a10bad2d6247703c1a29199e5",
-"main.dart.js": "732dc465e165bf19e5a633bb74de39b4",
+  "version.json": "4b6db237b3514a88107a422469adfb0f",
+"index.html": "a82abbb6113c6449380815a70a1a3592",
+"/": "a82abbb6113c6449380815a70a1a3592",
+"main.dart.js": "fb8b019ac4ed95422eb68ff63c24c8fc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
-"assets/AssetManifest.json": "522bce206e3dc904e2ad7a30e548f023",
-"assets/NOTICES": "126779e16ca6d7b8b7db6c64cc2f611e",
+"assets/AssetManifest.json": "c251072d90922d43533b8d2a78396518",
+"assets/NOTICES": "865c4bd3a0be1a89c6ca6116df5c2844",
 "assets/FontManifest.json": "b7320a7ef5ad634fc22b163a16c8a309",
-"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
+"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/assets/images/visa.svg": "45180370eb48dfdbdf59b3be4a3c0b59",
 "assets/assets/images/image8.jpg": "143b8d86b86c4e9a9d534a2571644c9c",
 "assets/assets/images/image9.jpg": "d4bca3468f462344ec7848569d428f6f",
@@ -38,9 +39,11 @@ const RESOURCES = {
 "assets/assets/pages/switch.json": "769aeaf2027194176f82e5c4b973903d",
 "assets/assets/pages/aspect_ratio.json": "1a48ae48bf258aaf4d0fbfb21d9a67f8",
 "assets/assets/pages/conditional.json": "f9ce65ff0a4aab6dab91d0dcf36b9c9c",
+"assets/assets/pages/interactive_viewer.json": "14e65ff967c97908bb9ee30130c6e078",
 "assets/assets/pages/opacity.json": "03db9231df110e1534e83cd0759cbc94",
 "assets/assets/pages/animated_theme.json": "adb0f9ea1f196a0b683fcda6776b3aa8",
-"assets/assets/pages/theme.json": "86a29ed2e2a50ed5d9b39bcc2a5384c3",
+"assets/assets/pages/intrinsic_width.json": "b2bd3074dd64f3840eee72211a6476cf",
+"assets/assets/pages/theme.json": "c82c608548c26d7ca149b7808f1005b2",
 "assets/assets/pages/animated_size.json": "ce28354b96fd51d1d3beaeefc1f03917",
 "assets/assets/pages/issue_12.json": "dbeb3dfda2da7418710ae5b6af35e66c",
 "assets/assets/pages/simple_page.json": "218a96e4287cb0ac7b31fc5769013697",
@@ -49,25 +52,34 @@ const RESOURCES = {
 "assets/assets/pages/animated_physical_model.json": "efbee6f0e3f91cecca4b60079e00b1d6",
 "assets/assets/pages/align.json": "b485f4a4f73d9326d3c001ecd391febe",
 "assets/assets/pages/animated_positioned.json": "2d45ba8a0001a14c2380f56c6a5ec420",
+"assets/assets/pages/limited_box.json": "d789904eeeb04a93ddd1f916b8e6cfcf",
+"assets/assets/pages/fractional_translation.json": "bd3b6afe62d8f9ce632ced50c5a4e52c",
 "assets/assets/pages/animated_align.json": "b62c4603f5dc3e644c212133c8e7c690",
+"assets/assets/pages/fractionally_sized_box.json": "ac4c7ae9ff95d797918016e57515d595",
 "assets/assets/pages/images.json": "a1d2f8925f0a6b26dbe1478deaee9eca",
 "assets/assets/pages/circular_progress_indicator.json": "4f696c1ada9aae3eef306dca0274a0dd",
 "assets/assets/pages/cupertino_switch.json": "6b66e7e2e55beefb09d9929406ee5db1",
 "assets/assets/pages/checkbox.json": "178ec66d1d4f5af1dcca14da46a1e3f4",
+"assets/assets/pages/placeholder.json": "17c389915fdc7caf0a565a7176dae1d9",
 "assets/assets/pages/issue_10.json": "3f94f674fa3036ee3ad561edca56f4fb",
 "assets/assets/pages/animated_default_text_style.json": "291bcccd308f88621717cc3337874710",
 "assets/assets/pages/bank_example.json": "115d08071a934c2e118ca367bbb19e5c",
 "assets/assets/pages/baseline.json": "3fe9abf09a5af2695fd73c3692113a22",
 "assets/assets/pages/tween_animation.json": "4b0e7efb6c633c58e9177d9a513d4618",
 "assets/assets/pages/card.json": "bcc0bdee15e86e0b3db7536fc6ef2336",
+"assets/assets/pages/intrinsic_height.json": "ec2247d1c677996d7c2b77c71135c404",
 "assets/assets/pages/directionality.json": "6835c8e2dd7c38f2851732b53e51c6ec",
 "assets/assets/pages/animated_switcher.json": "9e08303fe8f252f217e06c41b79d723a",
 "assets/assets/pages/clips.json": "6475dfb17f85b218e76183a83a293069",
 "assets/assets/pages/radio.json": "2eb9a65fcc9b8de02a9a048828b3bd30",
+"assets/assets/pages/issue_20_single.json": "31d96ed15fca633d7aa220197c102060",
 "assets/assets/pages/issue_11.json": "314308857d863065aed0aebb2907e4e8",
-"assets/assets/pages/asset_images.json": "0eb4d64685fea3898fb8208b8af82279",
+"assets/assets/pages/asset_images.json": "39a8f965d7439e229de7731893dc54de",
 "assets/assets/pages/animated_opacity.json": "a58012c2c8269ab8c7a94744efde4747",
 "assets/assets/pages/animated_positioned_directional.json": "67559e78bf9eebed7b317b330ac8f56c",
+"assets/assets/pages/offstage.json": "98f245ed0cb1a3f861a9b96d65a6ba24",
+"assets/assets/pages/overflow_box.json": "fb08fc868d15965009f4820335484c44",
+"assets/assets/pages/issue_20_list.json": "a3dad9cf46904eb0f4d7a7a5abbe3367",
 "assets/assets/pages/animated_container.json": "873f183537c011e2fff2390d88cfb65b",
 "assets/assets/pages/animated_padding.json": "570130d1d57adc8ac3fa4bcb37e246fb",
 "assets/assets/pages/linear_progress_indicator.json": "aa27d81870d32a4170afcf0143d841d5",
@@ -75,10 +87,12 @@ const RESOURCES = {
 "assets/assets/pages/animated_cross_fade.json": "bd47018b8ca3f4cd5d48695c0e86cc32",
 "assets/assets/pages/form.json": "5cec2875b2162e52ade426b73de3f8b1",
 "assets/assets/pages/list_view.json": "ffc64a252307dd45c79d9c9c6bb61eaa",
+"assets/assets/pages/buttons.json": "3d5111eb96c4c983d151c1afa97cb330",
 "assets/assets/pages/fitted_box.json": "99c069d207b4253f3193e5365b8d32ed",
 "assets/assets/pages/gestures.json": "3323d8abcf022fc28584227db8cbb937",
 "assets/assets/pages/input_error.json": "0c2394bf170ae3e93fd44a7bf1975d4b",
-"assets/assets/pages/center.json": "b9fae0ffc52b196ed2a8107113d2e794"
+"assets/assets/pages/center.json": "b9fae0ffc52b196ed2a8107113d2e794",
+"assets/assets/pages/popup_menu_button.json": "12a875f9c7aa3569e1bb2b1f515d6cb1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -90,13 +104,13 @@ const CORE = [
 "assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
-
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      // Provide a 'reload' param to ensure the latest version is downloaded.
-      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'reload'})));
+      return cache.addAll(
+        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
     })
   );
 });
@@ -111,7 +125,6 @@ self.addEventListener("activate", function(event) {
       var tempCache = await caches.open(TEMP);
       var manifestCache = await caches.open(MANIFEST);
       var manifest = await manifestCache.match('manifest');
-
       // When there is no prior manifest, clear the entire cache.
       if (!manifest) {
         await caches.delete(CACHE_NAME);
@@ -125,7 +138,6 @@ self.addEventListener("activate", function(event) {
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
         return;
       }
-
       var oldManifest = await manifest.json();
       var origin = self.location.origin;
       for (var request of await contentCache.keys()) {
@@ -163,24 +175,33 @@ self.addEventListener("activate", function(event) {
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
   var origin = self.location.origin;
   var key = event.request.url.substring(origin.length + 1);
   // Redirect URLs to the index.html
-  if (event.request.url == origin || event.request.url.startsWith(origin + '/#')) {
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
     key = '/';
   }
-  // If the URL is not the RESOURCE list, skip the cache.
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
   if (!RESOURCES[key]) {
-    return event.respondWith(fetch(event.request));
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
   }
   event.respondWith(caches.open(CACHE_NAME)
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache. Ensure the resources are not cached
-        // by the browser for longer than the service worker expects.
-        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
-        return response || fetch(modifiedRequest).then((response) => {
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
           cache.put(event.request, response.clone());
           return response;
         });
@@ -193,11 +214,12 @@ self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
   if (event.data === 'skipWaiting') {
-    return self.skipWaiting();
+    self.skipWaiting();
+    return;
   }
-
-  if (event.message === 'downloadOffline') {
+  if (event.data === 'downloadOffline') {
     downloadOffline();
+    return;
   }
 });
 
@@ -220,4 +242,26 @@ async function downloadOffline() {
     }
   }
   return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
 }
