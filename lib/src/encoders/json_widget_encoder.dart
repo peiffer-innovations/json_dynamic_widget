0da910d2b12901ac02cff74a47729f28210844ea
crisperit <krzysztof.perchlicki@gmail.com>
2023-05-08 09:27:01 +0200

    fix: Refactor

--- lib/src/encoders/json_widget_encoder.dart
+++ lib/src/encoders/json_widget_encoder.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'json_widget_encoders.dart';
+
 abstract class JsonWidgetEncoder<T extends Widget> {
   bool support(Type type);
 
@@ -7,11 +9,23 @@ abstract class JsonWidgetEncoder<T extends Widget> {
 
   dynamic encodeArgs(T widget);
 
-  dynamic encode(T widget) {
-    return {
+  dynamic encode(Element widgetElement) {
+    final widget = widgetElement.widget;
+    final jsonWidget = {
       'id': widget.key != null ? widget.key.toString() : '',
       'type': getType(),
-      'args': encodeArgs(widget),
+      'args': encodeArgs(widget as T),
     };
+    final childrenJsonWidgets = [];
+    widgetElement.visitChildElements((childElement) {
+      final childEncoder = JsonWidgetEncoders.getForWidget(childElement.widget);
+      childrenJsonWidgets.add(childEncoder.encode(childElement));
+    });
+    if (childrenJsonWidgets.length == 1) {
+      jsonWidget['child'] = childrenJsonWidgets[0];
+    } else if (childrenJsonWidgets.length > 1) {
+      jsonWidget['children'] = childrenJsonWidgets;
+    }
+    return jsonWidget;
   }
 }
--- test/encoders/json_widget_encoding_root_test.dart
+++ test/encoders/json_widget_encoding_root_test.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:json_dynamic_widget/src/encoders/json_widget_encoders_widget.dart';
+import 'package:json_dynamic_widget/src/encoders/json_widget_encoding_root.dart';
 
 void main() {
-  testWidgets('JsonWidgetEncodersWidget generation test', (WidgetTester tester) async {
+  testWidgets('JsonWidgetEncodingRoot generation test', (WidgetTester tester) async {
     const expected =
         '{"id":"","type":"column","args":{"crossAxisAlignment":"center","mainAxisAlignment":"start","mainAxisSize":"max","textBaseline":null,"textDirection":null,"verticalDirection":"down"},"children":[{"id":"","type":"align","args":{"alignment":"bottomCenter","heightFactor":2.0,"widthFactor":1.0}},{"id":"","type":"align","args":{"alignment":"bottomLeft","heightFactor":1.0,"widthFactor":1.0}}]}';
-    final encoderWidget = JsonWidgetEncodersWidget(
+    final encoderWidget = JsonWidgetEncodingRoot(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: const [
