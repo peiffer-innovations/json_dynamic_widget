20acefd9c9f279dd90a8bbeb581d720a7aa26c5d
action <noop@github.com>
2023-12-26 01:53:27 +0000

    Pages 218

--- web/main.dart.js
+++ web/main.dart.js
@@ -43427,7 +43427,7 @@ $1(a){var s=$.dB
 s=(s==null?$.dB=A.iv(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/54a7145303f0dd9d0f93424a2e124eb4abef5091/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/3f3e560236539b7e2702f5ac790b2a4691b32d49/":s)+a},
 $S:26}
 A.b1d.prototype={
 $1(a){this.a.remove()
