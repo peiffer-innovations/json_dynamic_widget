bcfa71fdcd1ebd509bc10ee6b3d31b3c88fcbe03
action <noop@github.com>
2024-05-27 17:13:35 +0000

    [actions skip]: Pages 232

--- web/main.dart.js
+++ web/main.dart.js
@@ -44443,7 +44443,7 @@ A.b1W.prototype={
 $1(a){var s=A.eX().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/f6344b75dcf861d8bf1f1322780b8811f982e31a/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/55eae6864b296dd9f43b2cc7577ec256e5c32a8d/":s)+a},
 $S:31}
 A.b2a.prototype={
 $1(a){this.a.remove()
