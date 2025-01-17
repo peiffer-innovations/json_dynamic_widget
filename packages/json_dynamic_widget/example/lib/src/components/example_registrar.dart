16b622e6d1b8bc08d5bce6b697b42da15104382a
Jeff Peiffer <jpeiffer@gmail.com>
2025-01-16 19:17:19 -0500

    trying to fix build

--- packages/json_dynamic_widget/example/lib/src/components/example_registrar.dart
+++ packages/json_dynamic_widget/example/lib/src/components/example_registrar.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: unused_import
 
-import 'package:example/src/dotted_border_builder.dart';
-import 'package:example/src/issue_219_page.dart';
-import 'package:example/src/svg_builder.dart';
 import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+import 'package:json_dynamic_widget_example/src/dotted_border_builder.dart';
+import 'package:json_dynamic_widget_example/src/issue_219_page.dart';
+import 'package:json_dynamic_widget_example/src/svg_builder.dart';
 
 part 'example_registrar.g.dart';
 
